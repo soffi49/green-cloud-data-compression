7162da17f25671a7a08ad0945b57b43a7896737c
soffi49 <zosia.wrona49@gmail.com>
2022-07-03 22:44:45 +0200

    [GC] Moved constants to constant class

--- engine/src/main/java/runner/service/domain/ScenarioConstants.java
+++ engine/src/main/java/runner/service/domain/ScenarioConstants.java
@@ -2,6 +2,8 @@ package runner.service.domain;
 
 public class ScenarioConstants {
 
+    public static final String RESOURCE_SCENARIO_PATH = "./scenarios/";
+
     public static final int CLIENT_NUMBER = 100;
     public static final int MAX_JOB_POWER = 30;
     public static final int MIN_JOB_POWER = 10;
