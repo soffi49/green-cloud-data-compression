96e16c5324c7db39bb8e8102f64b1d42ac70bc96
soffi49 <zosia.wrona49@gmail.com>
2022-08-28 04:11:42 +0200

    [GC-5] Refactored power shortage behaviour in cloud network

--- application/src/main/java/agents/cloudnetwork/behaviour/powershortage/handler/HandleJobTransferToServer.java
+++ application/src/main/java/agents/cloudnetwork/behaviour/powershortage/handler/HandleJobTransferToServer.java
@@ -1,29 +1,31 @@
 package agents.cloudnetwork.behaviour.powershortage.handler;
 
+import static agents.cloudnetwork.behaviour.powershortage.handler.logs.PowerShortageCloudHandlerLog.SERVER_TRANSFER_EXECUTE_TRANSFER_LOG;
 import static common.TimeUtils.getCurrentTime;
-
-import agents.cloudnetwork.CloudNetworkAgent;
-import domain.job.Job;
-import domain.job.PowerShortageJob;
-import jade.core.AID;
-import jade.core.Agent;
-import jade.core.behaviours.WakerBehaviour;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.time.OffsetDateTime;
 import java.util.Date;
 import java.util.Objects;
 
-/**
- * Behaviour is responsible for updating internal cloud network state according to the job transfer
- */
-public class TransferJobToServer extends WakerBehaviour {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-	private static final Logger logger = LoggerFactory.getLogger(TransferJobToServer.class);
+import agents.cloudnetwork.CloudNetworkAgent;
+import domain.job.Job;
+import domain.powershortage.PowerShortageJob;
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.WakerBehaviour;
+
+/**
+ * Behaviour updates internal cloud network state according to the job transfer
+ */
+public class HandleJobTransferToServer extends WakerBehaviour {
+
+	private static final Logger logger = LoggerFactory.getLogger(HandleJobTransferToServer.class);
 
 	private final CloudNetworkAgent myCloudNetworkAgent;
+	private final String guid;
 	private final String jobId;
 	private final AID newServer;
 
@@ -31,44 +33,45 @@ public class TransferJobToServer extends WakerBehaviour {
 	 * Behaviour constructor.
 	 *
 	 * @param myAgent      agent executing the behaviour
-	 * @param transferTime time when the power shortage starts
+	 * @param transferTime time when the power shortage begin
 	 * @param jobId        unique identifier of the job
 	 * @param newServer    server which will take over the job execution
 	 */
-	private TransferJobToServer(Agent myAgent, Date transferTime, String jobId, AID newServer) {
+	private HandleJobTransferToServer(Agent myAgent, Date transferTime, String jobId, AID newServer) {
 		super(myAgent, transferTime);
 		this.myCloudNetworkAgent = (CloudNetworkAgent) myAgent;
+		this.guid = myAgent.getName();
 		this.jobId = jobId;
 		this.newServer = newServer;
 	}
 
 	/**
-	 * Method creates the behaviour based on the passed arguments
+	 * Method creates the HandleJobTransferToServer behaviour based on the passed arguments
 	 *
 	 * @param cloudNetworkAgent cloud network executing the behaviour
 	 * @param powerShortageJob  job to be transferred
 	 * @param newServer         server which will take over the job execution
 	 * @return behaviour which transfer the jobs between servers
 	 */
-	public static TransferJobToServer createFor(final CloudNetworkAgent cloudNetworkAgent,
+	public static HandleJobTransferToServer createFor(final CloudNetworkAgent cloudNetworkAgent,
 			final PowerShortageJob powerShortageJob, AID newServer) {
 		final OffsetDateTime transferTime = getCurrentTime().isAfter(powerShortageJob.getPowerShortageStart()) ?
 				getCurrentTime() :
 				powerShortageJob.getPowerShortageStart();
-		return new TransferJobToServer(cloudNetworkAgent, Date.from(transferTime.toInstant()),
+		return new HandleJobTransferToServer(cloudNetworkAgent, Date.from(transferTime.toInstant()),
 				powerShortageJob.getJobInstanceId().getJobId(), newServer);
 	}
 
 	/**
-	 * Method transfers the job between servers. It is responsible for updating the internal state of the cloud network
+	 * Method transfers the job between servers.
+	 * It updates the internal state of the cloud network
 	 */
 	@Override
 	protected void onWake() {
 		final Job jobToExecute = myCloudNetworkAgent.manage().getJobById(jobId);
 		if (Objects.nonNull(jobToExecute)) {
-			logger.info("[{}] Updating the internal state of the cloud network", myCloudNetworkAgent.getName());
+			logger.info(SERVER_TRANSFER_EXECUTE_TRANSFER_LOG, guid, jobId, newServer.getLocalName());
 			myCloudNetworkAgent.getServerForJobMap().replace(jobId, newServer);
 		}
-		myAgent.removeBehaviour(this);
 	}
 }
