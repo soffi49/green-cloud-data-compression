73039436a0d1896c8570476b8eabf4216fad515e
soffi49 <zosia.wrona49@gmail.com>
2024-01-12 03:31:38 +0100

    [DC] Add jai core dependency

--- compile/compile-modules/initialize-backend.sh
+++ compile/compile-modules/initialize-backend.sh
@@ -17,6 +17,9 @@ mvn install:install-file -Dfile='.\lib\jade.jar' -DgroupId='com.tilab.jade' -Dar
 echo "Installing modified Jai codec dependency..."
 mvn install:install-file -Dfile='.\lib\codec.jar' -DgroupId='com.sun.media' -DartifactId=jai_codec -Dversion='1.1.3' -Dpackaging=jar &&
 
+echo "Installing modified Jai core dependency..."
+mvn install:install-file -Dfile='.\lib\jai_core.jar' -DgroupId='javax.media' -DartifactId=jai_core -Dversion='1.1.3' -Dpackaging=jar &&
+
 echo "Initializing database..."
 (source ./knowledge-database/run_database.sh || echo "Database could not be initialized") &&
 
